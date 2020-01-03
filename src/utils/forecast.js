859a4fbf6266239a0b9865a68dfcad7dad34386f
Jay <chowsing.jay@gmail.com>
2020-01-03 22:56:31 +0800

    add highest/lowest temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,6 @@ const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/c0ff887693895d63aeb3f046b43beab9/' + latitude + ',' + longitude + '?units=si'
-    // console.log('latitude: '+ latitude + ' logitude: ' + longitude)
     request({ url, json: true }, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
@@ -10,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
             callback('Unable to find location.Try another search.', undefined)
         }
         else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + ' chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + ' chance of rain.'
+            + 'The highest temperature for today is ' + body.daily.data[0].temperatureMax + ' degrees. And the lowest temperature for today is ' + body.daily.data[0].temperatureMin + ' degrees.' )
         }
     })
 }
